3ff4bcc538d690aa29a2cb93eedf8c129ffc58ac
Jordan Werthman <jordanwerthman@gmail.com>
2026-09-07 21:29:35 -0700

    CHECK crash on invalid spans in ErrorCollector
    ErrorCollector has an annoying habit of printing tons of blank space
    (and potentially crashing CLion's terminal) when an invalid span is
    used to print... Adding CHECKs allows catching these cases in a cleaner
    way.

--- compiler/error_collector.cc
+++ compiler/error_collector.cc
@@ -31,7 +31,9 @@ void print_error(std::string_view path,
 
   std::string_view line = contents.substr(line_start, line_end - line_start);
 
+  CHECK_LT(line_start, metadata.column_range.start);
   size_t relative_offset = metadata.column_range.start - line_start;
+  CHECK_LT(metadata.column_range.start, metadata.column_range.end) << "Error: '" << message << "' has invalid span";
   size_t span_length = metadata.column_range.end - metadata.column_range.start;
 
   std::string header = std::string(prefix) + ": " + std::string(path) + ":" +
